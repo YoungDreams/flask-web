968f353fadd0c94b2ef6ecab98712b5c77aaa4f1
taylordurden <258266864@qq.com>
2019-03-11 11:12:12 +0800

    user avatar and profile

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,9 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+import time
+from app.forms import EditProfileForm
 
 
 @app.route('/')
@@ -63,3 +66,44 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'},
+    ]
+    user.last_seen = _utc2local(user.last_seen)
+    return render_template('user.html', user=user, posts=posts)
+
+def _utc2local(utc_st):
+    """UTC时间转本地时间（+8: 00）"""
+    now_stamp = time.time()
+    local_time = datetime.fromtimestamp(now_stamp)
+    utc_time = datetime.utcfromtimestamp(now_stamp)
+    offset = local_time - utc_time
+    local_st = utc_st + offset
+    return local_st
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
