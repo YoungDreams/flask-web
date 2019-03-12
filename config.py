978fd9c898be84870180253e088fd484d54a83d9
Taylor <258266864@qq.com>
2019-03-13 00:30:03 +0800

    pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['258266864@qq.com']
+    POSTS_PER_PAGE = 25
